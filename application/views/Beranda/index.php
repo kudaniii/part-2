1ae30d702c5035777a84543ad43cf237b69417cb
daniik <akungame2046@gmail.com>
2024-10-03 04:28:25 +0700

    part 3

---
+++ application/views/Beranda/index.php
@@ -0,0 +1,97 @@
+
+<!--header-->
+<div class ="container">
+  <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="assets/images/kucing 1.gif" class="d-block w-100" height="590" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/images/kucing 2.jpg" class="d-block w-100" height="590" alt="...">
+    </div>
+    <div class="carousel-item">
+      <img src="assets/images/kucing 3.jpg"class="d-block w-100" height="590" alt="...">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
+<!-- end header-->
+
+<!--about-->
+<section class="about" id="about"> 
+<div class="container">
+  <div class="row mt-5">
+      <div class="col-md-12 text-center">
+        <h2>About</h2>
+      <hr>
+    </div>
+  </div>
+    <div class="row justify-content-center">
+   <div class="col-md-5">
+     <p> lorem, ipsum dolar sit amet consectetur adipicing elit.Ipsa, dolorum provident beatae,minima placeatmaxime cupiditate! Quisquam,eaque voluptates nihil labore rem perferendis tempore delectus distinctio minus maxime,Nexue,tempora,</p>
+  </div>
+   <div class="col-md-5">
+     <p> lorem, ipsum dolar sit amet consectetur adipicing elit.Ipsa, dolorum provident beatae,minima placeatmaxime cupiditate! Quisquam,eaque voluptates nihil labore rem perferendis tempore delectus distinctio minus maxime,Nexue,tempora,</p>
+   </div>  
+</div>
+</div>
+</section>
+
+      
+<!-- end about--> 
+
+<!-- kontak --> 
+<section class="kontak" id="kontak"> 
+  <div class="container">
+  <div class="row mt-5">
+      <div class="col-md-12 text-center">
+           <h2>Kontak</h2>
+           <hr>
+      </div>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-md-5">
+    <form>
+  <div class="from-group">
+    <label for="Nama"> Nama</label>
+    <input type="Text" class="form-control" id="Nama" placeholder="Masukan Nama" =>
+  </div>
+  <div class="from-group">
+    <label for="Nama">Email</label>
+    <input type="Text" class="form-control" id="Email" placeholder="Masukan Email" =>
+  </div>
+  <div class="from-group">
+    <label for="Nama">Alamat</label>
+    <input type="Text" class="form-control" id="Alamat" placeholder="Masukan Alamat" =>
+  </div>
+  <div class="from-group mb-3">
+    <label for="Nama">Pesan</label>
+    <textarea type="Textarea" class="form-control" id="Pesan" placeholder="Masukan Pesan" =></textarea>
+  </div>
+  <button type="submit" class="btn btn-primary">Kirim</button>
+</form>
+  </div>
+</div>
+</div>
+</div>
+
+  </section>
+
+<!-- end kontak --> 
+
+
+
+    
